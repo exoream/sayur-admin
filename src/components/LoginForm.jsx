7999432dbf54e09d3eb76212ebfcce8b9d511d1a
exoream <shiigetsu1@gmail.com>
2025-07-03 01:40:17 +0800

    fix: icon

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -86,7 +86,7 @@ const LoginForm = () => {
                         <div className="relative">
                             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <svg className="h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12H8m8 0l-4-4m4 4l-4 4m0-4H4" />
                                 </svg>
                             </div>
                             <input
